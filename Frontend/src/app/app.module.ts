a258962263e6b887ba188ebb6de533e7b30ef95e
Chinmay Shringi <chinmay.shringi@credilio.in>
2023-03-24 18:53:32 +0530

    add angular

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+  
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+  
+import { PostModule } from './post/post.module';
+import { PostComponent } from './post/post.component';
+  
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PostModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
